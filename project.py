6c4fdf73d5ea694fcd25df441e39984d433a21c7
root <root@localhost.localdomain>
2016-09-05 03:20:17 +0530

    DAILY DATA WITH FILLS

--- project.py
+++ project.py
@@ -6,13 +6,14 @@ import numpy as np
 import urllib2
 import datetime
 
+
 stox = 'AAPL', 'GOOGL', 'MSFT' , 'AMZN' , 'EBAY'
 
 
 
 def fetchdata(stock):
     filename = stock+'.txt'
-    x = urllib2.urlopen("http://chartapi.finance.yahoo.com/instrument/1.0/"+stock+"/chartdata;type=quote;range=1y/csv")
+    x = urllib2.urlopen("http://chartapi.finance.yahoo.com/instrument/1.0/"+stock+"/chartdata;type=quote;range=1d/csv")
     val = x.read()
     splitsrc = val.split('\n')
 
@@ -29,22 +30,34 @@ def fetchdata(stock):
     
 def graph(stock) :
     filename = stock+'.txt'
-    date,close,high,low,_open,volume = np.loadtxt(filename,unpack = True , delimiter=',' , converters={0:mdates.strpdate2num('%Y%m%d')})
+    timestamp,close,high,low,_open,volume = np.loadtxt(filename, dtype='O,'+'f8,'*5, delimiter=',', unpack=True, converters={0: lambda d: datetime.datetime.fromtimestamp(float(d))})
+
     fig = plt.figure(figsize=(10,7))
     ax1 = plt.subplot2grid((40,4), (0,0), rowspan=40, colspan=40)
-    plt.title(stock)
-    plt.xlabel("Date")
-    plt.ylabel("Rate")
-    ax1.plot(date,close)
-    ax1.plot(date,_open)
 
-    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y%m%d'))	
     
-    print "The Graph for "+stock+ "is being plotted"
+    for label in ax1.xaxis.get_ticklabels():
+        label.set_rotation(45)
 
+
+    plt.title(stock)
+    plt.xlabel("Time")
+    plt.ylabel("Rate")
+    ax1.plot(timestamp, low, color='orange', label ='low')
+    ax1.plot(timestamp, high, color='green', label ='high')
+    ax1.plot(timestamp, _open, color='black', label ='open')
+    ax1.plot(timestamp, close,color='blue', label ='close')
+    fill = 'red'
+    fille = 'green'
+    ax1.fill_between(timestamp, high, high[0], where=(high < high[0]), facecolor=fill, edgecolor=fill,alpha = 0.5)
+    ax1.fill_between(timestamp, high, high[0], where=(high > high[0]), facecolor=fille, edgecolor=fille, alpha = 0.5)
+    print "The Graph for "+stock+ "is being plotted"
+    
+    plt.subplots_adjust(left=0.09 , bottom=0.14 , right=0.97 , top=0.94 , wspace=0.2 , hspace=0)
+    plt.legend()
     plt.grid(True)
     plt.show()
-
+    
 for each in stox:
     fetchdata(each)
     graph(each)
